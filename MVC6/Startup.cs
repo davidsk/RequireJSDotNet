5ddef41cf800e3aed032720611988115dc859d9f
Keith Davidson <keithdavidson@hotmail.com>
2016-06-30 08:44:25 +0100

    Updated to ASP.Net Core 1.0

--- MVC6/Startup.cs
+++ MVC6/Startup.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RequireJsNet.EntryPointResolver;
 
 namespace MVC6
 {
     public class Startup
     {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -16,18 +33,27 @@ namespace MVC6
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //app.Map("/application", (app1) => this.Configure1(app1, env, loggerFactory));
-            this.Configure1(app, env, loggerFactory);
+            app.Map("/application", (app1) => this.Configure1(app1, env, loggerFactory));
+            //this.Configure1(app, env, loggerFactory);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure1(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseIISPlatformHandler();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseStaticFiles();
-
-            app.UseDeveloperExceptionPage();
 
             app.UseMvc(routes =>
             {
@@ -43,15 +69,24 @@ namespace MVC6
                     );
             });
 
+            app.Run(async (context) =>
+            {
+                var urlPatterns = new Dictionary<string, string>();
+                urlPatterns.Add("hostRelative", "/bar");
+                urlPatterns.Add("appRelativeExplicit", context.Request.PathBase + "/bar");
+                urlPatterns.Add("appRelativeImplicit", "~/bar");
+                urlPatterns.Add("relative", "../bar");
 
+                var message = string.Empty;
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+                foreach (var kvp in urlPatterns)
+                {
+                    message +=
+                        $"{kvp.Key}: {DefaultEntryPointResolver.ResolvePathToAppRoot(kvp.Value, context.Request.PathBase)}\n";
+                }
+
+                await context.Response.WriteAsync(message);
+            });
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
